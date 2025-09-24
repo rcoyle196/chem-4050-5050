478987697e51b4fb3a332a3957b64fc004228d6d
rcoyle196 <58236764+rcoyle196@users.noreply.github.com>
2025-09-23 23:12:40 -0700

    Add files via upload

--- homework-3-2/troutons_rule_optimization.py
+++ homework-3-2/troutons_rule_optimization.py
@@ -4,16 +4,20 @@ import numpy as np
 from scipy import stats
 from scipy.optimize import minimize
 from lecture_07_regression import ols_slope, ols_intercept, ols
+from troutons_rule import sse, var, se_slope, se_intercept, conf_int_intercept, conf_int_slope
 
+#read and set values for data fram
 df = pd.read_csv("C:\\Users\\rcoyl\\OneDrive\\Documents\\git_hub_wexler\\chem-4050-5050\\trouton.csv")
 x = df['T_B (K)']
 y = df['H_v (kcal/mol)']*4184 #convert to j/mol
 
+#def function for minimize function
 def objective(parm):
     a, b = parm
     res = y - (a*x + b)
     return np.sum(res ** 2)
 
+#find best fit line using minimize function
 result = minimize(
     fun= objective, # function to minimize
     x0 = [103,0], # initial guess
@@ -21,12 +25,25 @@ result = minimize(
     tol=1e-6 # tolerance for termination
 )
 
+#call the ols functions
 ols_slope(x,y)
 ols_intercept(x,y)
 ols(x,y)
 
+# get slope and intercept from minimize function and y
 a, b = result["x"]
 y_min = (a*x) + b
+
+res = y_min - a
+sse(res)
+var(res)
+se_intercept(x, res)
+se_slope(x, res)
+conf_int_intercept(x, res, confidence_level=.95)
+conf_int_slope(x, res, confidence_level=.95)
+
+print(f"\nslope and confidence interval {a:.3f} +/- {conf_int_slope(x, res, 0.95):.3f}")
+print(f"\nintercept and confidence interval {b/1000:.3f} +/- {conf_int_intercept(x, res, 0.95):.3f}")
 
 #find the slope using the functions and generate a line
 slope, intercept = ols(x,y)
@@ -66,16 +83,15 @@ plt.scatter(metal_x, metal_y, color = 'grey', label = 'metals')
 plt.plot(x, y_min, color = 'purple', label = 'min (Hv = a * TB + b)' )
 plt.text(
     x.max() * 0.7, y.min() * 1.05,
-    f"a = {a:.2f} J/mol·K\n"
-    f"b = {b/1000:.2f} kJ/mol",
+    f"a = {a:.3f} J/mol·K ± {conf_int_slope(x, res, 0.95):.3f}\n"
+    f"b = {b:.3f} kJ/mol ± {conf_int_intercept(x, res, 0.95):.3f}",
     fontsize=10,
     bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.5')
-    )
+)
 plt.title('Troutons Rule')
 plt.ylabel('H_v jol/mol')
 plt.xlabel('T_B boiling point in kelvin')
 plt.grid(True)
 plt.legend()
 plt.savefig('C:\\Users\\rcoyl\\OneDrive\\Documents\\git_hub_wexler\\chem-4050-5050\\homework3_2\\Trouton_rule_optimization', dpi=300)
-
 plt.show()
